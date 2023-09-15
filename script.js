3077a66eec6b672c5dc8bd18e6618e2953b7a176
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-15 11:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// Get references to the div and <h1> elements
+const sizeInfoDiv = document.getElementById('sizeInfo');
+const sizeHeading = sizeInfoDiv.querySelector('h1');
+
+// Function to update the size information
+function updateSizeInfo() {
+  // Get the window's width and height
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  // Update the content of the <h1> tag
+  sizeHeading.textContent = `Width: ${windowWidth} and Height: ${windowHeight}`;
+}
+
+// Add an event listener for the "resize" event on the window
+window.addEventListener('resize', updateSizeInfo);
+
+// Initial update of size information
+updateSizeInfo();
